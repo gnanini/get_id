16043730698748e3dbb17a1be7517f7eb0c5726f
gnanini <gnanini@gmail.com>
2021-04-21 16:26:27 -0300

    Add files via upload

---
+++ get_id.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+def get_id(game_name):
+    url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+    response = requests.get(url)
+    text = json.loads(response.text)
+    dicionario = {}
+
+    for game in text['applist']['apps']:
+        dicionario[game['name'].lower()] = game['appid']
+
+    print(dicionario[game_name.lower()])
+
+    return dicionario[game_name.lower()]
+
+
+def test():
+    #get_id('MORDHAU')
+    get_id(input('Type a correct game name: '))
+    return
+
+
+test()
